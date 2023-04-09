b2dd6b8c4b5e6fea4ca01a27ef91953d40b14fc3
rocklessg <abdulhafiz.suleiman@decagon.dev>
2023-04-09 21:35:01 +0100

    Home page and router implementations in progress

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Home() {
+  return (
+      <div>
+          <h1>Welcome To VPark</h1>
+          <p>Secure Parking space for your vehicle</p>
+          <p>Nothing does it better than VPark Application</p>
+    </div>
+  )
+}
+
+export default Home
